51415a25038c7a46a43dc9b8794176663ef809f0
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-01-12 22:07:45 +0100

    Added the function parsing + findmax()

--- src/main.c
+++ src/main.c
@@ -6,14 +6,16 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 20:31:44 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/12/05 14:24:26 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/12/07 17:36:52 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf/include/ft_printf.h"
+#include <stdlib.h>
+#include <limits.h>
+
+#include "ft_printf/ft_printf.h"
 #include "libft/libft.h"
 #include "inc/actions.h"
-#include <stdlib.h>
 
 void	visualize(t_list *lst_a, t_list *lst_b)
 {
@@ -48,44 +50,152 @@ void	visualize(t_list *lst_a, t_list *lst_b)
 	ft_printf("_ _\na b\n\n");
 }
 
-int	main(int argc, char **argv)
+void	my_free_all(char **arr)
 {
-	int		i;
-	int		*nb;
-	t_list	*lst_a = NULL;
-	t_list	*lst_b = NULL;
-	t_list	*elem;
+	int	i;
 
-	i = 1;
-	if (argc == 2)
-	while (i < argc)
+	i = 0;
+	while (arr[i] != NULL)
+		free(arr[i++]);
+	free(arr);
+}
+
+int	isvalidnumber(char *str)
+{
+	if (*str == '-' || *str == '+')
+		str++;
+	while (ft_isdigit(*str))
+		str++;
+	return (*str == '\0');
+}
+
+int	checkmaxint(char delim[], size_t lenmax, char str[])
+{
+	size_t	len;
+
+	len = ft_strlen(str);
+	if (len > lenmax)
+		return (0);
+	if (len == lenmax)
+	{
+		while (*delim != '\0')
+		{
+			if (*str > *delim)
+				return (0);
+			if (*str != *delim)
+				return (1);
+			str++;
+			delim++;
+		}
+	}
+	return (1);
+}
+
+int	isvalidint(char *str)
+{
+	char	sign;
+	char	*delim;
+	int		status;
+
+	sign = 0;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = 1;
+		str++;
+	}
+	while (*str == '0')
+		str++;
+	if (*str == '\0')
+		return (1);
+	if (sign == 0)
+		delim = ft_itoa(INT_MAX);
+	else
+		delim = ft_itoa(INT_MIN);
+	status = checkmaxint(delim + sign, ft_strlen(delim) - sign, str);
+	free(delim);
+	return (status);
+}
+
+int	findmax(t_list *lst)
+{
+	int	max;
+	int	i;
+	int	index;
+
+	max = INT_MIN;
+	i = 0;
+	index = i;
+	while (lst)
+	{
+		if (*(int *)lst->content >= max)
+		{
+			max = *(int *)lst->content;
+			index = i;
+		}
+		lst = lst->next;
+		i++;
+	}
+	return (index);
+}
+
+t_list	*dothing(char **argv, int i, int size)
+{
+	t_list	*lst_a;
+	int		*nb;
+
+	lst_a = NULL;
+	while (i < size)
+	{
+		if (!isvalidnumber(argv[i]) || !isvalidint(argv[i]))
+		{
+			ft_putstr_fd("Error\n", 2);
+			return (NULL);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < size)
 	{
 		nb = malloc(sizeof(int));
 		*nb = ft_atoi(argv[i]);
-		elem = ft_lstnew(nb);
-		ft_lstadd_front(&lst_a, elem);
-		ft_printf("[%s] ", argv[i]);
+		if (!nb)
+		{
+			ft_lstclear(&lst_a, &free);
+			return (NULL);
+		}
+		ft_lstadd_front(&lst_a, ft_lstnew(nb));
 		i++;
 	}
-	visualize(lst_a, lst_b);
-	ft_printf("sa :\n");
-	swap_elem(&lst_a);
-	visualize(lst_a, lst_b);
-	ft_printf("pb pb pb :\n");
-	push_elem(&lst_b, &lst_a);
-	push_elem(&lst_b, &lst_a);
-	push_elem(&lst_b, &lst_a);
-	visualize(lst_a, lst_b);
-	ft_printf("ra rb :\n");
-	rotate_elem(&lst_a);
-	rotate_elem(&lst_b);
-	visualize(lst_a, lst_b);
-	ft_printf("rra rrb :\n");
-	reverse_rotate_elem(&lst_a);
-	reverse_rotate_elem(&lst_b);
-	visualize(lst_a, lst_b);
+	visualize(lst_a, NULL);
+	ft_printf("max : %d\n", findmax(lst_a));
+	return (lst_a);
+}
 
-	ft_lstclear(&lst_a, &free);
-	ft_lstclear(&lst_b, &free);
+int	main(int argc, char **argv)
+{
+	int		i;
+
+	if (argc < 2)
+	{
+		ft_putstr_fd("Error\n", 2);
+		return (1);
+	}
+	i = argc != 2;
+	if (!i)
+	{
+		argv = ft_split(argv[1], ' ');
+		if (argv == NULL)
+		{
+			ft_putstr_fd("Error\n", 2);
+			return (1);
+		}
+		argc = 0;
+		while (argv[argc] != NULL)
+			argc++;
+	}
+	dothing(argv, i, argc);
+	if (!i)
+		my_free_all(argv);
 	return (0);
 }
