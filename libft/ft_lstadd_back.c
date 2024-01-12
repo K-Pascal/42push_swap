6d7ff3fa548d226db6b46ef3ae17529c5edb9d9d
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-01-12 22:07:45 +0100

    functions ft_lstadd* don't need a temp variable

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:28:42 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/12 18:19:18 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/12/05 19:25:26 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*last;
-
 	if (*lst == 0)
 	{
 		*lst = new;
 		return ;
 	}
-	last = ft_lstlast(*lst);
-	last->next = new;
+	ft_lstlast(*lst)->next = new;
 }
--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 17:23:52 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/12 18:19:48 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/12/05 19:25:06 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*first;
-
-	first = *lst;
-	new->next = first;
+	new->next = *lst;
 	*lst = new;
 }
